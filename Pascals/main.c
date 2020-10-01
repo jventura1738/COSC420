3f194a005ac105b4aa4fef2b6401b915780fa36d
Blaine-Mason <bmason3@gulls.salisbury.edu>
2020-09-30 21:38:06 -0400

    This is gonna be a long one

--- Pascals/main.c
+++ Pascals/main.c
@@ -1,15 +1,29 @@
+#include <stddef.h>
 #include <stdio.h>
 
-int C(int n, int k) {
-  double res = 1;
-  int i;
+size_t C(size_t n, size_t k) {
+  size_t res = 1;
+  size_t i;
   for(i = 1; i <=k; ++i){
     res = res * (n - k + i) / i;
   }
-  return (int)(res + 0.01);
+  return (size_t)(res + 0.01);
 }
 
 int main(){
-  printf("%d\n", C(5, 2));
+  size_t i, j;
+  size_t n = 1000000000;
+  size_t count = 0;
+  for(i = 0; i < n; i++){
+    for(j = 0; j <= i; j++){
+      printf("%ld\n", i);
+      if (C(i, j) % 7 != 0){
+        //printf("%ld\n", C(i,j));
+	count += 1;
+      }
+    }
+    printf("%ld", count);
+    puts("");
+  }
   return 0;
 }
