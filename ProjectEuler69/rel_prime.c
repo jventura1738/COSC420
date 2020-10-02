23f9240981cab609f37f902269971a62879a4268
Justin Ventura <justinv9001@aol.com>
2020-10-02 15:01:31 -0400

    update

--- ProjectEuler69/rel_prime.c
+++ ProjectEuler69/rel_prime.c
@@ -7,14 +7,39 @@
 #include <time.h> 
 
 int is_rel_prime(int *N, int *k);
+int totient_of(int *N);
 
 int main(int argc, char ** argv) {
 
-    int N = 30;
-    int k = 14;
+    int N = 10000;
 
-    int result = is_rel_prime(&N, &k);
-    printf("%d and %d relatively prime: %d\n", N,  k, result);
+    float * totient_list = malloc((N - 2)*sizeof(float));
+
+    int i, counter = 0;
+    float max = 0.0;
+    for (i = 2; i <= N; i++) {
+
+        totient_list[i-2] = ((i*1.0) / totient_of(&i));
+        if ((i*1.0) / totient_of(&i) > max) {
+
+            max = ((i*1.0) / totient_of(&i));
+
+        }
+
+    }
+
+    for (i = 0; i <= N - 2; i++) {
+
+        if (totient_list[i] == max) {
+
+            printf("%d ", i + 2);
+            counter++;
+
+        }
+
+    }
+
+    puts("");
 
     return 0;
 
@@ -25,7 +50,7 @@ int is_rel_prime(int *N, int *k) {
     int i;
 
     // assume k is smaller than N.
-    for (i = 2; i < *k; i++) {
+    for (i = 2; i <= *k; i++) {
 
         if (*N % i == 0 && *k % i == 0) {
 
@@ -36,5 +61,23 @@ int is_rel_prime(int *N, int *k) {
     }
 
     return 1;
+
+}
+
+int totient_of(int *N) {
+
+    int i, totient = 0;
+
+    for (i = 1; i <= *N; i++) {
+
+        if (is_rel_prime(N, &i)) {
+
+            totient += 1;
+
+        }
+
+    }
+
+    return totient;
 
 }
