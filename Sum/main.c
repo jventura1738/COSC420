a806c900b4769919c6a1a41d478c9096c97c2c50
Blaine-Mason <bmason3@gulls.salisbury.edu>
2020-10-03 15:34:16 -0400

    Nice

--- Sum/main.c
+++ Sum/main.c
@@ -1,16 +1,16 @@
 #include <stddef.h>
 #include <stdio.h>
 #include "mpi.h"
-
-size_t C(size_t n, size_t k) {
-  size_t res = 1;
-  size_t i;
-  for(i = 1; i <=k; ++i){
-    res = res * (n - k + i) / i;
+#include <math.h>
+long double divisor(long double n){
+  long double i;
+  long double sum = 0;
+  for(i = 1; i <= n; i++){
+    if (fmod(n,i) == 0)
+      sum += i*i;
   }
-  return (size_t)(res + 0.01);
+  return sum;
 }
-
 int main(int argc, char** argv){
   MPI_Init(&argc, &argv); // pass through cli args
   int worldSize, myRank;
@@ -19,27 +19,33 @@ int main(int argc, char** argv){
   MPI_Comm world = MPI_COMM_WORLD;
   MPI_Comm_size(world, &worldSize);
   MPI_Comm_rank(world, &myRank); 
-  size_t i, j;
-  size_t n = 100000;
-  size_t count = 0;
+
+
+
+  long double n = 1000000;
+  long double Final_sum = 0;
+  long double i, temp;
+
   int range, low, high;
   range = n/worldSize;
   if(myRank == 0){
-    low = 0;
+    low = 1;
     high = low + range;
   }else{
     low = myRank*range;
     high = low + range;
   }
-  for(i = low; i < high; i++){
-    for(j = 0; j <= i; j++){
-      if (C(i, j) % 7 != 0){
-        	count += 1;
-      }
+
+
+
+  for(i = low; i <= high; i++){
+    temp = sqrtl(divisor(i));
+    if(temp - floor(temp) == 0){
+      Final_sum += i;
     }
   }
-  printf("Node: %d Rows: %d - %d\n", myRank, low, high);
-  printf("%d count is: %ld\n", myRank, count);
+  printf("Node: %d Numbers: %d - %d\n", myRank, low, high);
+  printf("%d count is: %Lf\n", myRank, Final_sum);
   MPI_Finalize();
   return 0;
 }
