424526e1dfd3cd5934ac186c2da6a0abbca3fc5c
Justin <justinv9001@aol.com>
2020-10-14 13:31:48 -0400

    update

---
+++ LABS/LAB3/matrix.h
@@ -0,0 +1,39 @@
+// Justin Ventura & Blaine Mason
+// COSC420 Lab 3: matrix.h
+
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<math.h>
+#include<mpi.h>
+
+// NB: the A here needs to be a struct, not a pointer!
+#define INDEX(A,i,j) A->cols*i + j
+// now can do A.data[INDEX(A,i,j)]
+
+// use the above to shortcut accessing the array
+#define ACCESS(A,i,j) A->data[INDEX(A,i,j)]
+
+// C needs the "typedef"
+typedef struct {
+  int rows, cols; // dimensions
+  double* data;   // pointer to the data, a flat array
+} matrix;
+
+/*
+ * Matrix fillers and operations.
+*/
+void initRandMatrix(matrix *A, int rows, int cols);
+void initMatrix(matrix *A, int rows, int cols);
+void printMatrix(matrix *A);
+matrix transpose(matrix* A);
+double * addMatrix(matrix *A, matrix *B, MPI_Comm world, int worldSize, int myRank);  
+double * subtractMatrix(matrix *A, matrix *B, MPI_Comm world, int worldSize, int myRank);
+double * multiplyMatrix(matrix *A, matrix *B, MPI_Comm world, int worldSize, int myRank);
+double innerProduct(matrix* A, matrix *B, MPI_Comm world, int worldSize, int myRank);
+void gauss_jordan(matrix* A, matrix *b, MPI_Comm world, int worldSize, int myRank);
+
+#endif
