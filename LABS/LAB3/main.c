6cbf10f495bdba8c7d4134a9bedd43e917524303
Justin <justinv9001@aol.com>
2020-10-18 23:41:17 -0400

    update

--- LABS/LAB3/main.c
+++ LABS/LAB3/main.c
@@ -37,7 +37,7 @@ int main(int argc, char ** argv) {
   */
 
   // TODO: Perform alg.
-  int n =6;
+  int n = 10;
   matrix A, b, aI, I, temp;
   matrix newb;
   initMatrix(&aI, n, n);
@@ -50,14 +50,17 @@ int main(int argc, char ** argv) {
     //printMatrix(&A);
     //printMatrix(&b);
   }
+
   newb.data = gauss_jordan(&A, &b, world, worldSize, myRank);
+
   if(myRank == 0){
     puts("");
     printf("The new b vector is:\n");
     printMatrix(&newb);
     puts("");
   }
-  /*
+
+  
   aI.data = gauss_jordan(&A, &I, world, worldSize, myRank);
   if(myRank == 0){
     printf("The A inverse:\n");
@@ -70,28 +73,123 @@ int main(int argc, char ** argv) {
     printMatrix(&temp);
     puts("");
   }
-  */
+  
+
+
+  /* File stuff for b*/
+  MPI_File fh;
+
+  int * send_cnts = malloc(sizeof(int) * worldSize);
+  int * disp_cnts = malloc(sizeof(int) * worldSize);
+
+  int i, disp = 0;
+  for (i = 0; i < worldSize; i++) {
+
+    send_cnts[i] = n/worldSize;
+    disp_cnts[i] = disp;
+    disp += n/worldSize;
+
+  }
+  if (n % worldSize > 0) {
+
+    send_cnts[worldSize-1] += n % worldSize; 
+
+  }
+
+  double * local_m = malloc(sizeof(double) * send_cnts[myRank]);
+
+  MPI_Scatterv(newb.data, send_cnts, disp_cnts, MPI_DOUBLE, local_m, send_cnts[myRank], MPI_DOUBLE, 0, world);
+  MPI_Offset offset = myRank * sizeof(double) * send_cnts[myRank];
+
 
   // hexdump -v -e '5/4 "%3d"' -e '"\n"'  datafile
 
-  MPI_File fh;
-  int offset = myRank * sizeof(double) * (n / worldSize);
-
-
-  // if (myRank == worldSize - 1 && n % worldSize > 0) {
-
-
-
-  // }
-
-  MPI_File_open(world, "outfile",
+  MPI_File_open(world, "outfile1",
       MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &fh);
 
-  /* NEW VERSION */
-  MPI_File_set_view(fh, offset, MPI_DOUBLE, MPI_DOUBLE, "native", MPI_INFO_NULL);
-  MPI_File_write(fh, newb.data, 3, MPI_DOUBLE, MPI_STATUS_IGNORE);
+  MPI_File_write_at(fh, offset, local_m, send_cnts[myRank], MPI_DOUBLE, MPI_STATUS_IGNORE);
 
   MPI_File_close(&fh);
+
+  free(send_cnts);
+  free(disp_cnts);
+  free(local_m);
+
+  /* File stuff for AInv */
+
+  send_cnts = malloc(sizeof(int) * worldSize);
+  disp_cnts = malloc(sizeof(int) * worldSize);
+
+  disp = 0;
+  for (i = 0; i < worldSize; i++) {
+
+    send_cnts[i] = n*n/worldSize;
+    disp_cnts[i] = disp;
+    disp += n*n/worldSize;
+
+  }
+  if (n*n % worldSize > 0) {
+
+    send_cnts[worldSize-1] += n*n % worldSize; 
+
+  }
+
+  local_m = malloc(sizeof(double) * send_cnts[myRank]);
+
+  MPI_Scatterv(aI.data, send_cnts, disp_cnts, MPI_DOUBLE, local_m, send_cnts[myRank], MPI_DOUBLE, 0, world);
+  offset = myRank * sizeof(double) * send_cnts[myRank];
+
+
+  // hexdump -v -e '5/4 "%3d"' -e '"\n"'  datafile
+
+  MPI_File_open(world, "outfile2",
+      MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &fh);
+
+  MPI_File_write_at(fh, offset, local_m, send_cnts[myRank], MPI_DOUBLE, MPI_STATUS_IGNORE);
+
+  MPI_File_close(&fh);
+
+  free(send_cnts);
+  free(disp_cnts);
+  free(local_m);
+
+  /* File stuff for A*IAnv */
+
+  send_cnts = malloc(sizeof(int) * worldSize);
+  disp_cnts = malloc(sizeof(int) * worldSize);
+
+  disp = 0;
+  for (i = 0; i < worldSize; i++) {
+
+    send_cnts[i] = n*n/worldSize;
+    disp_cnts[i] = disp;
+    disp += n*n/worldSize;
+
+  }
+  if (n*n % worldSize > 0) {
+
+    send_cnts[worldSize-1] += n*n % worldSize; 
+
+  }
+
+  local_m = malloc(sizeof(double) * send_cnts[myRank]);
+
+  MPI_Scatterv(temp.data, send_cnts, disp_cnts, MPI_DOUBLE, local_m, send_cnts[myRank], MPI_DOUBLE, 0, world);
+  offset = myRank * sizeof(double) * send_cnts[myRank];
+
+
+  // hexdump -v -e '5/4 "%3d"' -e '"\n"'  datafile
+
+  MPI_File_open(world, "outfile3",
+      MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &fh);
+
+  MPI_File_write_at(fh, offset, local_m, send_cnts[myRank], MPI_DOUBLE, MPI_STATUS_IGNORE);
+
+  MPI_File_close(&fh);
+
+  free(send_cnts);
+  free(disp_cnts);
+  free(local_m);
 
   MPI_Finalize();
 
