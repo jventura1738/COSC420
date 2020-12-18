ef0e1f9c3706a42176678e76d0575574baa654bd
Blaine-Mason <bmason3@gulls.salisbury.edu>
2020-12-18 01:07:39 -0500

    Done, Readme is all

--- Project-3/parallel.py
+++ Project-3/parallel.py
@@ -5,9 +5,10 @@ import math
 import copy
 from itertools import permutations
 from mpi4py import MPI
+from tqdm import tqdm
 import pickle
 
-
+# Vertex Class
 class Vertex:
     def __init__(self, x, y):
         self.x = x
@@ -68,7 +69,7 @@ class Graph:
             total += self.edges[i][2]
         return total
 
-
+# A method to provide information about a tour given the nodes
 def describe_tour(g, start):
     temp_graph = copy.deepcopy(g)
     tour = []
@@ -93,14 +94,14 @@ def describe_tour(g, start):
     temp_graph.plot()
     return temp_graph
 
-
+# Calculates the total tour length
 def get_distance(nodes):
     total = 0
     for i in range(0, len(nodes)):
         total += nodes[i - 1].distance(nodes[i])
     return total
 
-
+# Brute force solution to TSP
 def bf(nodes):
     min_length = get_distance(nodes)
     min_path = nodes
@@ -111,7 +112,7 @@ def bf(nodes):
             min_path = path
     return min_path
 
-
+# Serial SA Method
 def anneal(nodes):
     initial_temp = 90
     final_temp = .1
@@ -128,32 +129,8 @@ def anneal(nodes):
     return solution
 
 
-def co_operation(best_solution, current_solution, rank, worldsize, world):
-    if(rank == 0):
-        print(f"Rank {rank} has sent best_solution: {get_distance(best_solution)} to 1")
-        world.send(best_solution, 1)
-        return best_solution
-    else:
-        best_solution_rec = world.recv(source=rank - 1)
-        print(f"Rank {rank} has recieved best_solution: {get_distance(best_solution_rec)}")
-        best_solution_j = copy.copy(best_solution)
-        current_solution_j = copy.copy(current_solution)
-        if(get_distance(best_solution_rec) < get_distance(best_solution)):
-            best_solution_j = copy.deepcopy(best_solution_rec)
-            current_solution_j = copy.deepcopy(best_solution_rec)
-        if(rank < worldsize - 1):
-            # print(f"rank: {rank} world_size: {worldsize}")
-            world.send(best_solution_j, rank + 1)
-        return current_solution_j
-
-
+# Annealing step for parallel implementation
 def anneal_step(current_solution, best_solution, current_temp):
-    """
-    a = rand.randint(0, len(current_solution) - 2)
-    b = a + 1
-    new_solution = copy.deepcopy(current_solution)
-    new_solution[a], new_solution[b] = current_solution[b], current_solution[a]
-    """
     new_solution = np.random.permutation(current_solution)
     delta = get_distance(current_solution) - get_distance(new_solution)
     if((delta > 0) or (np.random.uniform(0, 1) < np.exp((delta/current_temp)))
@@ -170,17 +147,20 @@ def main():
     world_size = comm.Get_size()
     with open("test.txt", "rb") as fp:   # Unpickling
         verts = pickle.load(fp)
-    print(verts)
+    sum_ = 0
     graph = Graph()
     for i in range(0, len(verts)):
         graph.add_node(verts[i])
-    # print(f"hello from proc {rank}")
-    L = int((5*5000)/world_size)
+
+    E = 5000
+    L = int((5*E)/world_size)
     initial_solution = np.random.permutation(graph.nodes)
     current_solution = initial_solution
     best_solution = initial_solution
     t = 90
     current_temp = t
+    if(rank == world_size - 1):
+        pbar = tqdm(total=t)
     while(current_temp > .1):
         for i in range(1, L):
             best_solution = anneal_step(current_solution, best_solution, t)
@@ -194,12 +174,17 @@ def main():
                 current_solution = copy.deepcopy(best_solution_rec)
             if(rank < world_size - 1):
                 comm.isend(best_solution, rank + 1)
+        if(rank == world_size - 1):
+            pbar.update(.995)
         current_temp -= .995
+        if(rank == world_size - 1):
+            pbar.close()
+            pbar.clear()
+    # Print Results
     if(rank == world_size - 1):
         anneal_ = Graph()
         for i in range(0, len(best_solution)):
             anneal_.add_node(best_solution[i])
-        print(anneal_)
         anneal_graph = describe_tour(anneal_, 0)
         print(anneal_graph.get_total_distance())
 
